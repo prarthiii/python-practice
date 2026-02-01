23fe241889eb90a3df682d1ba139c4420b458d77
Prarthita <laprarthita@gmail.com>
2026-02-01 10:44:35 +0530

    calculating first occurence in a string

---
+++ firstOccurence.py
@@ -0,0 +1,20 @@
+# mapping = {
+# 'haystack' : 'sadbutsad',
+# 'needle' : 'sad'
+# }
+
+
+haystack = 'hello'
+needle = 'll'
+
+def occur():
+
+    for i in range(len(haystack) - len(needle) + 1 ):
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break
+            else:
+                return i
+    return -1
+
+print(occur())
