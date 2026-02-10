72843990edf58128cefbb44d5a666bc1add3239c
Prarthita <laprarthita@gmail.com>
2026-02-10 12:44:00 +0530

    finding target in a sorted array

---
+++ target.py
@@ -0,0 +1,18 @@
+nums = [1,3]
+target = 1
+
+ans = []
+
+for i, val in enumerate(nums):
+    if val == target:
+        ans.append(i)
+
+if not ans:
+    ans = [-1, -1]
+
+ans = [ans[0], ans[-1]]
+
+
+    
+print(ans)
+# print(len(ans))
