41c20e9a54e144e69afb48ca7da6d35619040fa1
Prarthita <laprarthita@gmail.com>
2026-02-17 15:03:00 +0530

    finding digital root without loops

---
+++ addDigits.py
@@ -0,0 +1,13 @@
+nums = 38
+
+# while nums >= 10:
+#     tot = 0
+#     for n in str(nums):
+#         tot += int(n)
+#     nums = tot
+
+# print(nums)
+
+# this code also has a sol which can be executed even without loops
+
+print(1 + (nums-1) % 9)
