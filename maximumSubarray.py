3185cb9e4107b7ba52eb4319699cfca65ad6dde9
Prarthita <laprarthita@gmail.com>
2026-03-25 15:03:58 +0530

    finding the maximum sum of a subarray

---
+++ maximumSubarray.py
@@ -0,0 +1,10 @@
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+current = nums[0]
+maximum = nums[0]
+
+for i in range(1, len(nums)):
+    current = max(nums[i], current + nums[i])
+    maximum = max(current, maximum)
+
+print(maximum)
