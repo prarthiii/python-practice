312481145da61e341bde6e199065637ba2420a6e
Prarthita <laprarthita@gmail.com>
2026-03-29 10:39:36 +0530

    removing target nodes in a linked list

---
+++ removeNodes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+
+        curr = dummy
+
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next  
+            else:
+                curr = curr.next
+        return dummy.next
