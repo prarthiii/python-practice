a5dd016c22ede5595f97b79d702b536418e85b7e
Prarthita <laprarthita@gmail.com>
2026-04-26 10:42:27 +0530

    finding the peak element

---
+++ peakElement.py
@@ -0,0 +1,15 @@
+nums = [1,2,3,1]
+
+left = 0
+right = len(nums)-1
+
+while left < right:
+    mid = (left + right) // 2
+
+    if nums[mid] < nums[mid + 1]:
+        left = mid + 1
+
+    else:
+        right = mid
+
+print(left)
