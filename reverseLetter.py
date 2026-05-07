c2a23261d6c245b875c8c5b4b52492972bce5a5a
Prarthita <laprarthita@gmail.com>
2026-05-07 12:16:23 +0530

    reversing only alphabets in a string ( special characters position remains unchanged)

---
+++ reverseLetter.py
@@ -0,0 +1,20 @@
+s = "Test1ng-Leet=code-Q!"
+
+s = list(s)
+
+i = 0
+j = len(s) -  1
+
+while i < j:
+    if not s[i].isalpha():
+        i += 1
+
+    elif not s[j].isalpha():
+        j -= 1
+
+    else:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+print("".join(s))
